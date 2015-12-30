f658146cfcbc177a38d7166ebcff9fab896b8d1f
Brenton Simpson <appsforartists@google.com>
2015-12-29 17:33:52 -0800

    [fixed] Layer props passthrough

--- src/components/Layer.jsx
+++ src/components/Layer.jsx
@@ -35,6 +35,7 @@ var Layer = React.createClass(
                                                             ...style,
                                                           }
                                                         }
+                                              { ...propsPassthrough }
                                             >                                    
                                               { children }
                                             </View>;
